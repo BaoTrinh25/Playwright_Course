28cdabbb1b6e329b763b2f491f71d914ba520782
BaoTrinh25 <baotrinh2505@gmail.com>
2025-02-05 15:27:22 +0700

    Test Data Generator - Section 8

--- Playwright_Section_4_5_6/pw-practice-app/tests/section-6/usePageObjects.spec.js
+++ Playwright_Section_4_5_6/pw-practice-app/tests/section-6/usePageObjects.spec.js
@@ -1,5 +1,7 @@
 import {test} from "@playwright/test"
 import { PageManager } from '../../page-objects/pageManager'
+import {faker} from '@faker-js/faker'
+
 
 test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/', { timeout: 40000, waitUntil: 'domcontentloaded' })
@@ -16,12 +18,14 @@ test('navigate to form page', async({page}) => {
 
 test('parametrized methods', async({page}) => {
     const pm = new PageManager(page)
+    const randomFullName = faker.person.fullName()
+    const randomEmail = `${randomFullName.replace(' ', '')}${faker.number.int(1000)}@test.com`
 
     await pm.navigateTo().formLayout()
-    await pm.onFormLayoutsPage().submitUsingTheGridFormatWithCredentialsAndSelectOption('test@test.com', 'Welcom1', 'Option 1')
-    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox('Jonh Smith', 'John@test.com', true)
-    await pm.navigateTo().datepicker()
-    await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(10)
-    await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 15)
+    await pm.onFormLayoutsPage().submitUsingTheGridFormatWithCredentialsAndSelectOption(randomEmail, 'Welcom1', 'Option 1')
+    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, true)
+    // await pm.navigateTo().datepicker()
+    // await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(10)
+    // await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 15)
 
 })
